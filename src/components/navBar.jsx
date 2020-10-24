1c05b6c7f7b0c014a3f881b7f3b9e3a94edd2e98
luka <lukicha.kobaidze@gmail.com>
2020-10-24 16:42:55 +0400

    Testing Mode

---
+++ src/components/navBar.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { NavLink, Link } from "react-router-dom";
+
+const navBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/movies">
+        Movie Generator
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <NavLink className="nav-link" to="/movies">
+            Movies <span className="sr-only">(current)</span>
+          </NavLink>
+          <NavLink className="nav-link" to="/customers">
+            Customers
+          </NavLink>
+          <NavLink className="nav-link" to="/rentals">
+            Rentals
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default navBar;
